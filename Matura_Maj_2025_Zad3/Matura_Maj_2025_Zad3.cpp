32f31fd3ac0b2144a6e82ea61da402a155e989d0
Emilia <emilia3d.pulawy@gmail.com>
2025-10-03 23:48:15 +0200

    Dodaj pliki projektów.

---
+++ Matura_Maj_2025_Zad3/Matura_Maj_2025_Zad3.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <numeric>
+#include <cmath>
+
+using namespace std; 
+
+ifstream fin("dron_przyklad.txt");
+
+int main()
+{
+	cout << "Zad.3.1\n";
+	int a,b; 
+	while (fin>>a>>b ) // automatycznie czyta dwie liczby z linii
+		if (gcd (abs(a),abs(b))>1)
+		cout << a << ";" << b << endl;
+
+	
+
+
+
+}
+
